cd67485a12b94414e91e36b384e806feb727441e
lilbiji <pranav.agarw@gmail.com>
2021-04-13 07:00:17 +0530

    added handlebars to render dynamic content instead of static web pages, only boilerplate added

--- src/app.js
+++ src/app.js
@@ -6,19 +6,27 @@ console.log(__dirname)
 const app= express()
 const publicDirectoryPath=path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help',(req,res)=>{
-    res.send([{
-        name:'Pranav',
-        age:21
-    },{
-        name:'Sara'
-    }])
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'WEATHER APP',
+        name: 'Pranav Agarwal'
+    })
 })
 
 app.get('/about',(req,res)=>{
-    res.send('<h1>About</h1>')
+    res.render('about',{
+        title:'ABOUT',
+        name:'Pranav Agarwal'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        message:'This is the help page!'
+    })
 })
 
 app.get('/weather',(req,res)=>{
